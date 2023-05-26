90b84326a21c04ee47a24843f36f869de171fcb5
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-26 07:05:56 -0700

    :

---
+++ test/main.h
@@ -0,0 +1,20 @@
+#ifndef _PRINTF_H
+#define _PRINTF_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
+#define OUTPUT_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+#define NULL_STRING "(null)"
+
+#define PARAMS_INIT (0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+#define CONVERT_LOWERCASE 1
+#define CONVERT_UNSIGNED 2
+
+
