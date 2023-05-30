ba3fda5a5a90df2ccb4cb43d02d45c1456dc3794
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-30 07:22:33 -0700

    :

--- simple_printers.c
+++ simple_printers.c
@@ -45,3 +45,33 @@ return (sum);
 }
 /**
  * print_rot13 - prints string in rot
+ * @ap: string
+ * @params: the parameters struct
+ *
+ * Return: number bytes printed
+ */
+int print_rot13(va_list ap, params_t *params)
+{
+int i, index;
+int count = 0;
+char arr[] =
+	"NOPQRSTUVWXYZABCDEFGHIJKLM	nopqrstuvwxyzabcdefghijklm";
+char *a = va_arg(ap, char *);
+(void)params;
+
+i = 0;
+index = 0;
+while (a[i])
+{
+if ((a[i] >= 'A' && a[i] <= 'Z')
+|| (a[i] >= 'a' && a[i] <= 'z'))
+{
+index = a[i] - 65;
+count += putchar(arr[index]);
+}
+else
+count += putchar(a[i]);
+i++;
+}
+return (count);
+}
