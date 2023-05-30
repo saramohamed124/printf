b82d28876fa7ca67a809cdab494db978f413ec73
Asmaa <eng.asmaao2017@gmail.com>
2023-05-30 06:17:06 -0700

    a

--- main.h
+++ main.h
@@ -24,16 +24,73 @@ int _putchar(int c);
 int print_char(va_list ap, params_t *params);
 int print_string(va_list ap, params_t *params);
 int print_percent(va_list ap, params_t *params);
+int print_S(va_list ap, params_t *params);
+int print_int(va_list ap, params_t *params);
 
+char *convert(long int num, int base, int flags, params_t *params);
+int print_address(va_list ap, params_t *params);
+int print_unsigned(va_list ap, params_t *params);
+
+int (*get_specifier(char *s)) (va_list ap, params_t *params);
+int get_print_func(char *s, va_list ap, params_t *params);
+int get_flag(char *s, params_t *params);
+int get_modifier(char *s, params_t *params);
+char *get_width(char *s, va_list ap, params_t *params)
+
+int print_hex(va_list ap, params_t *params);
+int print_HEX(va_list ap, params_t *params);
+int print_binary(va_list ap, params_t *params);
+int print_octal(va_list ap, params_t *params);
+
+int  print_from_to(char *start, char *stop, char *except);
+int print_rev(va_list ap, params_t *params);
+int print_rot13(va_list ap, params_t *params);
+
+/**
+ * struct specifier - struct token
+ * @specifier: format
+ * @f: the function associated
+ */
 typedef struct specifier
 {
 	char *specifier;
 	int (*f)(va_list, params_t *);
 }specifier_t;
 
+/**
+ * struct parameters - struct parameters
+ * @unsign: plus_flag
+ * @plus_flag: plus_flag
+ * @space_flag: space_flag
+ * @width: width
+ * @minus_flag: minus_flag
+ * @zero_flag: zero_flag
+ * @hashtag_flag: hashtag_flag
+ * @precision: precision
+ * @h_modifier: h_modifier
+ * @l_modifier: l_modifier
+ */
 typedef struct parameters
 {
+        unsigned int unsign : 1;
+        unsigned int plus_flag : 1;
+        unsigned int space_flag : 1;
+        unsigned int hashtag_flag : 1;
+        unsigned int zero_flag : 1;
+        unsigned int minus_flag : 1;
+
+        unsigned int width;
 	unsigned int precision;
+	unsigned int h_modifier : 1;
+        unsigned int l_modifier : 1;
 }params_t;
+
+int _isdigit(int c);
+int _strlen(char *s);
+int print_number(char *str, params_t *params);
+int print_number_right_shift(char *str, params_t *params);
+int print_number_left_shift(char *str, params_t *params);
+
+char *get_precision(char *p, va_list ap, params_t *params);
 
 #endif
