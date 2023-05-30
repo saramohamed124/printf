8303b94e1289b302b80eee1a8c6c65db877ab614
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-30 15:11:40 -0700

    :

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _printf - prints anything
  * @format: the format string
@@ -7,42 +8,42 @@
 */
 int _printf(const char *format, ...)
 {
-int sum = 0;
-va_list ap;
-char *p, *start;
-params_t params = PARAMS_INIT;
+	int sum = 0;
+	va_list ap;
+	char *p, *start;
+	params_t params = PARAMS_INIT;
 
-va_start(ap, format);
+	va_start(ap, format);
 
-if (!format || (format[0] == '%' && !format[1]))
-return (-1);
-if (format[0] == '%' && format[1] == ' ' && !format[2])
-return (-1);
-for (p = (char *)format; *p; p++)
-{
-init_params(&params, ap);
-if (*p == '%')
-{
-sum += putchar(*p);
-continue;
-}
-start = p;
-p++;
-while (get_flag(p, &params))
-{
-p++;
-}
-p = get_width(p, &params, ap);
-p = get_precision(p, &params, ap);
-if (get_modifier(p, &params))
-p++;
-if (!get_specifier(p))
-sum += print_from_to(start, p,
-params.l_modifier || params.h_modifier ? p - 1 : 0);
-else
-sum += get_print_func(p, ap, &params);
-}
-putchar(BUF_FLUSH);
-va_end(ap);
-return (sum);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = (char *)format; *p; p++)
+	{
+		init_params(&params, ap);
+		if (*p != '%')
+		{
+			sum += putchar(*p);
+			continue;
+		}
+		start = p;
+		p++;
+		while (get_flag(p, &params))
+		{
+		p++;
+		}
+		p = get_width(p, &params, ap);
+		p = get_precision(p, &params, ap);
+		if (get_modifier(p, &params))
+		p++;
+		if (!get_specifier(p))
+			sum += print_from_to(start, p,
+				params.l_modifier || params.h_modifier ? p - 1 : 0);
+		else
+			sum += get_print_func(p, ap, &params);
+	}
+	putchar(BUF_FLUSH);
+	va_end(ap);
+	return (sum);
 }
