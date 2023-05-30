4d59560d74b27a47ebdcdcc65fd9ecdf68158130
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-30 09:23:02 -0700

    :

---
+++ print_number.c
@@ -0,0 +1,128 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if character
+ * @c: the character to check
+ *
+ * Return: 1 if degit, 0
+ */
+int _isdigit(int c)
+{
+return (c >= '0' && c <= '9');
+}
+
+/**
+ * _strlen - return the length
+ * @s: the string
+ *
+ * Return: interger
+ */
+int _strlen(char *s)
+{
+int i = 0;
+
+while (*s++)
+i++;
+return (i);
+}
+
+/**
+ * print_number - prints a number
+ * @str: the base
+ * @params: the parameter
+ *
+ * Return: chars printed
+ */
+int print_number(char *str, params_t *params)
+{
+unsigned int i = _strlen(str);
+int neg = (!params->unsign && *str == '-');
+
+if (!params->precision && *str == '0' && !str[1])
+str = "";
+if (neg)
+{
+str++;
+i--;
+}
+if (params->precision != UINT_MAX)
+while (i++ < params->precision)
+*--str = '0';
+if (neg)
+*--str = '-';
+if (!params->minus_flag)
+return (print_number_right_shift(str, params));
+else
+return (print_number_left_shift(str, params));
+}
+/**
+ * print_number_right_shift - prints a number
+ * @str: the base
+ * @params: the parameter
+ *
+ * Return: chars printed
+ */
+int print_number_right_shift(char *str, params_t *params)
+{
+unsigned int n = 0, neg, neg2, i = _strlen(str);
+char pad_char = ' ';
+
+if (params->zero_flag && !params->minus_flag)
+pad_char = '0';
+neg = neg2 = (!params->unsign && *str == '-');
+if (neg && i < params->width && pad_char == '0' && !params->minus_flag)
+str++;
+else
+neg = 0;
+if ((params->plus_flag && !neg2) ||
+(!params->plus_flag && params->space_flag && !neg2))
+i++;
+if (neg && pad_char == '0')
+n += putchar('_');
+if (params->plus_flag && !neg2 && pad_char == '0' && !params->unsign)
+n += putchar('+');
+else if (!params->plus_flag && params->space_flag && !neg2 &&
+!params->unsign && params->zero_flag)
+n += putchar(' ');
+while (i++ < params->width)
+n += putchar(pad_char);
+if (neg && pad_char == ' ')
+n += putchar('-');
+if (params->plus_flag && !neg2 && pad_char == ' ' && !params->unsign)
+n += putchar('+');
+else if (!params->plus_flag && params-> space_flag && !neg2 &&
+!params->unsign && !params->zero_flag)
+n += putchar(' ');
+n += _puts(str);
+return (n);
+}
+
+/**
+ * print_number_left_shift - prints a number
+ * @str: the base
+ * @params: the parameter
+ *
+ * Return: chars printed
+ */
+int print_number_left_shift(char *str, params_t *params)
+{
+unsigned int n = 0, neg, neg2, i = _strlen(str);
+char pad_char = ' ';
+
+if (params->zero_flag && !params->minus_flag)
+pad_char = '0';
+neg = neg2 = (!params->unsign && *str == '-');
+if (neg && i < params->width && pad_char == '0' && !params->minus_flag)
+str++;
+else
+neg = 0;
+
+if (params->plus_flag && !neg2 && !params->unsign)
+n += putchar('+'), i++;
+else if (params->space_flag && !neg2 && !params->unsign)
+n += putchar(' '), i++;
+n += _puts(str);
+while (i++ < params->width)
+n += putchar(pad_char);
+return (n);
+}
