5fb258393d4e4ea2e48886500c23fcf9b51b4d8c
saramohamed124 <saramohamaden1234@gmail.com>
2023-05-30 08:01:10 -0700

    :

--- number.c
+++ number.c
@@ -37,3 +37,45 @@ if (sign)
 *--ptr = sign;
 return (ptr);
 }
+
+/**
+ * print_unsigned - prints unsigned integer
+ * @ap: argument pointer
+ * @params: the parameters
+ *
+ * Return: bytes printed
+ */
+int print_unsigned(va_list ap, params_t *params)
+{
+unsigned long l;
+
+if (params->l_modifier)
+l = (unsigned long)va_arg(ap, unsigned long);
+else if (params->h_modifier)
+l = (unsigned short int)va_arg(ap, unsigned int);
+else
+l = (unsigned int)va_arg(ap, unsigned int);
+params->unsign = 1;
+return (print_number(convert(1, 10, CONVERT_UNSIGNED, params), params));
+}
+
+/**
+ * print_address - prints address
+ * @ap: argument pointers
+ * @params: the parameters
+ *
+ * Return: byters printed
+ */
+int print_address(va_list ap, params_t *params)
+{
+unsigned long int n = va_arg(ap, unsigned long int);
+char *str;
+
+if (!n)
+return (_puts("(nil)"));
+
+str = convert(n, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
+*--str = 'x';
+*--str = '0';
+return (print_number(str, params));
+}
